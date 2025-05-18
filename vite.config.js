91e6d8b67742042687d0d6c256d192c80afb8fff
shmp-dev <shmp.dev@gmail.com>
2025-05-18 23:09:54 +0900

    [mod]vite出力先をdocsに変更（pages公開用）

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,    // 開発サーバーのポート番号
+    open: true     // サーバー起動時に自動でブラウザを開く
+  },
+  build: {
+    outDir: 'docs', // ビルド成果物の出力先
+  }
+});
